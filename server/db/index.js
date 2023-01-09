198140096382849a530327def2bc1739671671f6
joe-leahy <josleahy@icloud.com>
2023-01-09 17:21:25 -0500

    Product Model Added

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,15 @@
-//this is the access point for all things database related!
-
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
 
-//associations could go here!
+User.hasMany(Product)
+Product.hasMany(User)
 
 module.exports = {
   db,
   models: {
     User,
+    Product
   },
 }
