0b0eba833ea1dbcef5c6ca98fc5fa37c68c930b4
Jessica O'Hanlon <jessica.ohanlon@gmail.com>
2023-01-15 17:17:36 -0500

    Admin functionality to add and remove products

---
+++ client/features/addProduct/AddProduct.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchSingleProductAsync,
+  addProductAsync,
+  selectSingleProduct,
+} from "../slices/singleProductSlice";
+import { useNavigate } from "react-router-dom";
+
+const AddProduct = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const product = useSelector(selectSingleProduct);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [imageurl, setImageurl] = useState("");
+
+  const handleAdd = async (evt) => {
+    evt.preventDefault();
+    const newProduct = { name, description, price, imageurl };
+    await dispatch(addProductAsync(newProduct));
+    navigate(`/products/${evt.target.value}`);
+  };
+
+
+  return (
+    <>
+      <h2>Add New Product Information: </h2>
+      <form id="editForm" onSubmit={handleAdd}>
+        <label htmlFor="addProduct">Name: </label>
+        <input
+          name="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="addProduct">Description: </label>
+        <input
+          description="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label htmlFor="addProduct">ImageUrl: </label>
+        <input
+          imageurl="imageurl"
+          value={imageurl}
+          onChange={(e) => setImageurl(e.target.value)}
+        />
+        <label htmlFor="addProduct">Price: </label>
+        <input
+          price="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button type="submit" className="editProductButton">
+          Submit{" "}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default AddProduct;
